7ca1c71229f65162c2324c965d9c20075d08cb8a
Rodrigo081pb <kauarodrigo1193@gmail.com>
2025-04-24 00:48:21 -0300

    Ajustes

--- src/app/pages/projects/site-vendas/loja-infoproduto/loja-infoproduto.component.ts
+++ src/app/pages/projects/site-vendas/loja-infoproduto/loja-infoproduto.component.ts
@@ -33,21 +33,21 @@ export class LojaInfoprodutoComponent {
       nome: 'Curso Angular Avançado',
       descricao: 'Aprofunde seus conhecimentos em Angular e crie apps de escala.',
       preco: 299.00,
-      imagem: 'https://i.pinimg.com/736x/ab/cd/ef/abcdef123456.jpg'
+      imagem: 'https://i.pinimg.com/236x/bf/3a/f7/bf3af74658c268a3ddbd4714ec79733c.jpg'
     },
     {
       id: 2,
       nome: 'E-book UX/UI Design',
       descricao: 'Guia completo de design de interfaces modernas.',
       preco: 49.00,
-      imagem: 'https://i.pinimg.com/736x/12/34/56/123456abcdef.jpg'
+      imagem: 'https://i.pinimg.com/236x/f1/d3/3f/f1d33fbe8a2475d7e9d290d1611a17fc.jpg'
     },
     {
       id: 3,
       nome: 'Template Next.js Profissional',
       descricao: 'Pacote de templates prontos para projetos corporativos.',
       preco: 199.00,
-      imagem: 'https://i.pinimg.com/736x/98/76/54/987654abcdef.jpg'
+      imagem: 'https://i.pinimg.com/236x/3f/0f/5a/3f0f5a8d4c668cde6512b7fd55128a0d.jpg'
     }
   ];
 
